63810312ce943225e0d844f015740f9fae4da396
jmllegrand <jml.legrand@gmail.com>
2016-10-23 13:07:30 +0200

    chore: setup project + first component

---
+++ src/app/app.module.ts
@@ -0,0 +1,16 @@
+/**
+ * Created by jmlegrand on 22/10/16.
+ */
+
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {App} from "./app";
+
+
+@NgModule({
+    imports: [BrowserModule],
+    declarations: [App],
+    bootstrap : [App]
+})
+
+export class AppModule {}
---
+++ src/app/app.ts
@@ -0,0 +1,16 @@
+/**
+ * Created by jmlegrand on 22/10/16.
+ */
+
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'app',
+    template: `
+        <div>
+            <h3>yo, hello world!</h3>
+        </div>
+    `
+})
+
+export class App {}
